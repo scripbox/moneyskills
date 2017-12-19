c60e821f88a20c3a6b6972410eb57a8c4b394d50
Krati Mishra <krati.mishra@scripbox.com>
2017-12-19 11:13:30 +0530

    Add mailtrain to moneyskills

--- money-skills.js
+++ money-skills.js
@@ -49,3 +49,88 @@ document.addEventListener("mozfullscreenchange", function() {
 document.addEventListener("webkitfullscreenchange", function() {
   if (!document.webkitIsFullScreen) player.stopVideo();
 }, false);
+
+$(document).ready(function(){
+  $('.subscribe-mailtrain-button').click(function(e){
+    subscribeEmailtoMailTrain();
+  });
+  $('input[type="number"]').keydown(function(e) {
+    var key = e.charCode || e.keyCode || 0;
+    // allow backspace, tab, delete, enter, arrows, numbers and keypad numbers ONLY
+    // home, end, period, and numpad decimal
+    return (
+      (key >= 48 && key <= 57));
+  });
+  $('.subscribe-mailtrain-mobile-input').keydown(function(e){
+    if isValidNumber($('.subscribe-mailtrain-mobile-input').val())
+      return true;
+    else
+      $('.subscribe-mailtrain-input-info').html('invalid number');
+  });
+});
+
+function subscribeEmailtoMailTrain(){
+  var subscriber_data={};
+  subscriber_data.email=$('.subscribe-mailtrain-email-input').val();
+  subscriber_data.name=$('.subscribe-mailtrain-name-input').val();
+  subscriber_data.source=$('.subscribe-mailtrain-button').data("source");
+  subscriber_data.mobile=$('.subscribe-mailtrain-mobile-input').val();
+  subscriber_data.organisation=$('.subscribe-mailtrain-company-input').val();
+  subscriber_data.designation=$('.subscribe-mailtrain-designation-input').val();
+
+  if ((!subscriber_data.email== '') && (!subscriber_data.name== '')) {
+    if(isValidEmail(subscriber_data.email)) {
+      $('.loading').show();
+      $.post("http://localhost:3000/moneyskills_subscribe", subscriber_data)
+        .done(function(response) {
+          $('.loading').hide();
+          $('.mailtrain-just-subscribed').hide();
+          if(response.success) {
+            $('.mailtrain-just-subscribed').show();
+            $('.subscribe-mailtrain-input-info').hide();
+          }
+          else {
+            if (response.message) {
+              $('.subscribe-mailtrain-input-info').html(response.message);
+            }
+            else {
+              $('.subscribe-mailtrain-input-info .mini-sip-validation-error').html('Something went wrong');
+            }
+            $('.mailtrain-just-subscribed').hide();
+            $('.loading').hide();
+          }
+        })
+        .error(function(error) {
+          $('.mailtrain-just-subscribed').hide();
+          $('.loading').hide();
+          if (response.message) {
+            $('.subscribe-mailtrain-input-info').html(response.message);
+          }
+          else {
+            $('.subscribe-mailtrain-input-info').html('Something went wrong');
+          }
+          $('.mailtrain-just-subscribed').hide();
+        });
+    }
+    else {
+      $('.subscribe-mailtrain-input-info').html('Enter Valid Email Id');
+      $('.mailtrain-just-subscribed').hide();
+    }
+  }
+}
+
+function isValidEmail(email){
+  var filter = /^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  if(filter.test(email)) {
+    return true;
+  }
+  return false;
+}
+
+function isValidNumber(mobile) {
+  var filter = /([^\d])\d{10}([^\d])/
+  if(filter.test(mobile)) {
+    return true;
+  }
+  return false;
+}
